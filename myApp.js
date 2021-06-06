41ffe190a1a2cf3e9c8b7ddc3213b16ccac58f74
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-06 13:58:32 +0530

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,38 +1,9 @@
-var express = require('express');
-var app = express();
-console.log('Hello World');
+const express = require("express");
+const app = express();
+console.log("Hello World");
 
+app.get("/", function (req, res) {
+  res.send("Response String");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
