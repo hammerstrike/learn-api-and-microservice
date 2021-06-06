c14de1a1888a419ed00d8ca1d47eebdae8fbf1b7
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-06 14:58:34 +0530

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -5,13 +5,16 @@ console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 
 /** 
- * 
  * Start a working express server
  * Serve request
 */
 const indexFile = __dirname + '/views/index.html';
 app.get("/", function (req, res) {
     res.sendFile(indexFile);
+});
+
+app.get('/json', function(req, res){
+    res.json({"message": "Hello json"});
 });
 
 module.exports = app;
