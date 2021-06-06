2956fd12148431b5af33bc48991694cb751b82c5
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-06 14:18:30 +0530

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 console.log("Hello World");
 
+const absolutePath = __dirname + '/views/index.html';
+
+/** 
+ * 
+ * Start a working express server
+ * Serve request
+ * app.get("/", function (req, res) {
+ *      res.send("Hello Express");
+ * });
+*/
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+    res.sendFile(absolutePath);
 });
 
 module.exports = app;
