b5d890f37c7bc0e6c948b8dc8de90e5757ce1e71
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-08 00:51:29 +0530

    Create a Model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// Schema
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age : Number,
+  favoriteFoods : [String]
+});
 
-
-let Person;
-
+let Person = mongoose.model('Person', personSchema);;
+console.log(Person);
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
