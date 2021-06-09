2d407b2f03a201e8c14690ccce0d21cc8180f3ad
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-09 23:42:27 +0530

    Hide Potentially Dangerous Information Using helmet.hidePoweredBy()

--- myApp.js
+++ myApp.js
@@ -1,20 +1,62 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const api = require("./server.js");
-const helmet = require("helmet");
+
+const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 
-app.use(express.static("public"));
-app.disable("strict-transport-security");
-app.use("/_api", api);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-module.exports = app;
