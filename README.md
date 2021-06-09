383b932d138546645e386338ae1352a7588843c5
Amardeep <amardeep.kamble.17@gmail.com>
2021-06-09 23:13:48 +0530

    Install and Require Helmet

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+# [Introduction to Information Security with HelmetJS Challenges](https://www.freecodecamp.org/learn/information-security/information-security-with-helmetjs/)
--- myApp.js
+++ myApp.js
@@ -1,61 +1,61 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-require("dotenv").config();
-console.log("Hello World");
 
-app.use("/public", express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}));
+const helmet = require('helmet');
 
-// Middelware
-app.use(function (req, res, next) {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-});
 
-/**
- * Start a working express server
- * Serve request
- */
-const indexFile = __dirname + "/views/index.html";
-app.get("/", function (req, res) {
-  res.sendFile(indexFile);
-});
 
-app.get("/json", function (req, res) {
-  let message = "Hello World";
-  res.json({
-    message:
-      process.env.MESSAGE_STYLE === "uppercase"
-        ? message.toUpperCase()
-        : message,
-  });
-});
 
-app.get(
-  "/now",
-  function (req, res, next) {
-    req.time = new Date().toString();
-    next();
-  },
-  function (req, res) {
-    res.json({ time: req.time });
-  }
-);
 
-app.get("/:word/echo", function (req, res) {
-  const { word } = req.params;
-  res.json({ echo: word });
-});
 
-app.get('/name', function (req, res) {
-    const { first, last } = req.query;
-    res.send({ name: `${first} ${last}`});
-})
 
-app.post('/name', function (req, res) {
-    const { first, last } = req.body;
-    res.send({ name: `${first} ${last}`});
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
